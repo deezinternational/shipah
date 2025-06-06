7f19135a1d5bc0b73516408b267bc8fdf617f0c0
deezinternational <145091485+deezinternational@users.noreply.github.com>
2025-06-06 14:42:19 -0500

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -24,13 +24,14 @@ def smart_address_split(address: str):
 
     # Regex: street, optional address2, city, state, zip
     pat = re.compile(
-        r"""^
-        (?P<street>[\d\w ./\-\#]+?)                                     
-        (?:,\s*(?P<address2>(Apt|Apartment|Suite|Ste|Unit|#|Bldg|Building|Floor|Fl|Rm|Room|Lot|Space|Dept|Trailer|Trlr|PO Box|P\.O\. Box|POB|Box)\s*[\w\-]+))?
-        ,?\s*(?P<city>[A-Za-z .'-]+)                                   
-        ,\s*(?P<state>[A-Z]{2})                                          
-        \s+(?P<zip>\d{5}(?:-\d{4})?)                                    
-        $""", re.IGNORECASE | re.VERBOSE)
+    r"""^
+    (?P<street>[\d\w .\#/\-]+?)
+    (?:,\s*(?P<address2>(Apt|Apartment|Suite|Ste|Unit|\#|Bldg|Building|Floor|Fl|Rm|Room|Lot|Space|Dept|Trailer|Trlr|PO Box|P\.O\. Box|POB|Box)\s*[\w\-]+))?
+    ,?\s*(?P<city>[A-Za-z .'-]+)
+    ,\s*(?P<state>[A-Z]{2})
+    \s+(?P<zip>\d{5}(?:-\d{4})?)
+    """, re.IGNORECASE | re.VERBOSE)
+
 
     m = pat.search(rest)
     if m:
